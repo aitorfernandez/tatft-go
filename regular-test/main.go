5722a18cf061bceb7af9ad68da09ec1851b719a4
Aitor Fernandez <aitor@qad.re>
2020-01-24 10:57:43 +0000

    add run for different scenarios

--- regular-test/main.go
+++ regular-test/main.go
@@ -6,6 +6,18 @@ func Hello() string {
 	return "Hello, world"
 }
 
+func Add(nums ...int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	var result int
+	for _, n := range nums {
+		result += n
+	}
+	return result
+}
+
 func main() {
 	fmt.Println(Hello())
 }
--- regular-test/main_test.go
+++ regular-test/main_test.go
@@ -12,9 +12,17 @@ func TestHello(t *testing.T) {
 		// t.Log("want", want)
 		// t.Fail()
 	}
+}
 
-	// or
-	// if got := Hello(); got != want {
-	// 	t.Errorf("got %q want %q", got, want)
-	// }
+func TestAdd(t *testing.T) {
+	// with scenarios
+	t.Run("return 0 with no params", func(t *testing.T) {
+		if got := Add(); got != 0 {
+			t.Errorf("got %q want 0", got)
+		}
+	})
+
+	if got := Add(1, 2, 3, 4); got != 10 {
+		t.Errorf("got %q want 10", got)
+	}
 }
